72cc89a32a9b39507689f0c6a0549386969300ff
Idris Sunmola <idrisun22@gmail.com>
2023-05-08 21:04:53 -0400

    transpose jacobian

--- UR5_Jac/trans_jacobian.m
+++ UR5_Jac/trans_jacobian.m
@@ -1,67 +1,62 @@
-function Jb_transpose = trans_jacobian(theta)
-    % ur5BodyJacobianTranspose - Calculate the transpose of the body Jacobian
-    % for a UR5 robot with given joint angles
-    %
+function TJ = ur5TJcontrol(g_d, K, ur5)
+    % ur5TJcontrol: Control UR5 to achieve the desired end-effector pose
+    % using the transpose Jacobian method.
     % Input:
-    %   theta - 6x1 vector of joint angles
-    %
-    % Output:
-    %   Jb_transpose - Transpose of the 6x6 body Jacobian matrix
+    %   g_d: 4x4 desired end-effector pose
+    %   K: control gain
+    %   ur5: UR5 robot object
 
-    % Define unit vectors
-    e1 = [1, 0, 0]';
-    e2 = [0, 1, 0]';
-    e3 = [0, 0, 1]';
+    % Initialize variables
+    T = 0.4;
+    q = ur5.get_current_joints();
+    g = ur5FwdKin(q);
+    xi = getXi(g_d \ g);
+    v = xi(1:3);
+    w = xi(4:6);
+    m = 20;
 
-    % Define rotational axes
-    w_1 = e3;
-    w_2 = e2;
-    w_3 = e2;
-    w_4 = e2;
-    w_5 = -e3;
-    w_6 = e2;
+    % Show desired frame
+    Frame_desired = tf_frame('base_link', 'Frame_desired', g_d);
 
-    % Define reference points
-    q1 = [0; 0; 0];
-    q2 = [0; 0; 0.0892];
-    q3 = [0.425; 0; 0.0892];
-    q4 = [0.425 + 0.392; 0; 0.0892];
-    q5 = [0.425 + 0.392; 0.1093; 0];
-    q6 = [0.425 + 0.392; 0.1093; 0.0892 - 0.09475];
+    % Continue until the desired pose is reached
+    while or(norm(v) > 0.003, norm(w) > pi/180)
+        qk = ur5.get_current_joints();
+        gk = ur5FwdKin(qk);
+        J = ur5BodyJacobian(qk);
+        xi = getXi(g_d \ gk);
+        v = xi(1:3);
+        w = xi(4:6);
+        dq = K * T * (J' * xi);
 
-    % Define initial transformation matrix
-    g_tool = [-1, 0, 0, 0.425 + 0.392;
-          0, 0, 1, 0.0825 + 0.1093;
-          0, 1, 0, 0.0892 - 0.09475;
-          0, 0, 0, 1];
+        % Adjust control input
+        if v > 0.5
+            xi = xi / 4;
+        end
+        if norm(xi) < 0.6
+            T = (xi' * J * J' * xi) / norm(J * J' * xi);
+        end
 
-    % Calculate transformation matrices for each joint
-    g1 = expm([SKEW3(w_1) -cross(w_1, q1); 0 0 0 0] * theta(1, 1));
-    g2 = expm([SKEW3(w_2) -cross(w_2, q2); 0 0 0 0] * theta(2, 1));
-    g3 = expm([SKEW3(w_3) -cross(w_3, q3); 0 0 0 0] * theta(3, 1));
-    g4 = expm([SKEW3(w_4) -cross(w_4, q4); 0 0 0 0] * theta(4, 1));
-    g5 = expm([SKEW3(w_5) -cross(w_5, q5); 0 0 0 0] * theta(5, 1));
-    g6 = expm([SKEW3(w_6) -cross(w_6, q6); 0 0 0 0] * theta(6, 1));
+        % Scale dq based on its magnitude
+        if 0.0001 < norm(dq) && norm(dq) < 0.02
+            dq = dq * 0.02 / norm(K * T * (J' * xi));
+            m = 5;
+        elseif norm(dq) < 0.0001
+            dq = dq * 0.001 / norm(K * T * (J' * xi));
+            m = 3;
+        end
 
-    % Calculate forward kinematics
-    f_kin = g1 * g2 * g3 * g4 * g5 * g6 * g_tool;
+        % Update joint angles
+        qk = qk - dq;
 
-    % Calculate spatial twists
-    Xi1 = [-cross(w_1, q1); w_1];
-    Xi2 = [-cross(w_2, q2); w_2];
-    Xi3 = [-cross(w_3, q3); w_3];
-    Xi4 = [-cross(w_4, q4); w_4];
-        Xi5 = [-cross(w_5, q5); w_5];
-    Xi6 = [-cross(w_6, q6); w_6];
+        % Check manipulability
+        if abs(manipulability(J, 'sigmamin')) < 0.0001
+            finalerr = -1;
+            return;
+        end
 
-    % Calculate the spatial Jacobian
-    spatial_j = [Xi1, Adj(g1) * Xi2, Adj(g1 * g2) * Xi3, Adj(g1 * g2 * g3) * Xi4, 
-    Adj(g1 * g2 * g3 * g4) * Xi5, Adj(g1 * g2 * g3 * g4 * g5) * Xi6];
-
-    % Calculate the body Jacobian
-    Jb = inv(Adj(f_kin)) * spatial_j;
-
-    % Calculate the transpose of the body Jacobian
-    Jb_transpose = Jb';
+        % Move robot and wait for motion to complete
+        t = max(abs(dq) / (ur5.speed_limit * pi)) * m;
+        ur5.move_joints(qk, t);
+        pause(t);
+    end
 end
-
